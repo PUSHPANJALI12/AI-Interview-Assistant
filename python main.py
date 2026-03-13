a8a0554138910dcfde7e24d92a1aff337c01d4b0
PUSHPANJALI12 <99067575+PUSHPANJALI12@users.noreply.github.com>
2026-03-14 01:13:10 +0530

    Create python main.py

---
+++ python main.py
@@ -0,0 +1,13 @@
+skills = input("Enter your skill: ")
+
+questions = {
+"python":"What is list comprehension?",
+"java":"What is JVM?",
+"data analyst":"What is data cleaning?"
+}
+
+if skills.lower() in questions:
+    print("Interview Question:")
+    print(questions[skills.lower()])
+else:
+    print("Skill not found. Try Python, Java, Data Analyst")
