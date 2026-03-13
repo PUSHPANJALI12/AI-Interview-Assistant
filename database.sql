5d61918f27436b77959ce7a66afb7b1aca2da7ca
PUSHPANJALI12 <99067575+PUSHPANJALI12@users.noreply.github.com>
2026-03-14 01:23:34 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE interview_questions (
+    id INT PRIMARY KEY,
+    skill VARCHAR(50),
+    question TEXT
+);
+
+INSERT INTO interview_questions VALUES
+(1,'python','What is list comprehension?'),
+(2,'java','What is JVM?'),
+(3,'sql','What is normalization?');
